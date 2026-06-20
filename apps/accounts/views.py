c9ee47c4616e2f52f777c01c920b5f68f6938d15
bmainmaster-coder <bmainmaster@gmail.com>
2026-06-20 15:37:30 -0500

    Your changes

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,14 +1,18 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 from django.views.decorators.http import require_POST
 
 from apps.accounts.forms import RecaptchaAuthenticationForm
 from apps.assets.models import Asset
 
 
+@method_decorator(never_cache, name="dispatch")
 class AppLoginView(LoginView):
     template_name = "accounts/login.html"
     form_class = RecaptchaAuthenticationForm
@@ -16,6 +20,13 @@ class AppLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy("accounts:dashboard")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        key = settings.RECAPTCHA_PUBLIC_KEY or ""
+        context["recaptcha_key_prefix"] = key[:12]
+        context["recaptcha_is_test_key"] = key.startswith("6LeIxAcT")
+        return context
 
 
 class AppLogoutView(LogoutView):
